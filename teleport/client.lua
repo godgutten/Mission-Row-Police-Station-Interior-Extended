963243ab845e3934e8e0d15a5394df450da51249
godgutten <godgutten0358@hotmail.com>
2019-01-19 12:56:48 +0100

    Added teleport script

---
+++ teleport/client.lua
@@ -0,0 +1,106 @@
+local TeleportFromTo = {	
+	["Mission Row Police Station Interior Extended1"] = {
+		positionFrom = { ['x'] = 452.13, ['y'] = -984.54, ['z'] = 26.67, nom = "Go to interrogation"},
+		positionTo = { ['x'] = 442.14, ['y'] = -997.4, ['z'] = 4.8, nom = "Go to main room"},
+	},
+	["Mission Row Police Station Interior Extended2"] = {
+		positionFrom = { ['x'] = 446.8, ['y'] = -986.37, ['z'] = 26.67, nom = "Go to briefing"},
+		positionTo = { ['x'] = 442.12, ['y'] = -986.69, ['z'] = 4.8, nom = "Go to main room"},
+	},
+	["Mission Row Police Station Interior Extended3"] = {
+		positionFrom = { ['x'] = 456.76, ['y'] = -985.94, ['z'] = 26.67, nom = "Go to storage"},
+		positionTo = { ['x'] = 456.37, ['y'] = -1000.73, ['z'] = 4.8, nom = "Go to main room"},
+	}	
+}
+
+Drawing = setmetatable({}, Drawing)
+Drawing.__index = Drawing
+
+
+function Drawing.draw3DText(x,y,z,textInput,fontId,scaleX,scaleY,r, g, b, a)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+
+    local scale = (1/dist)*20
+    local fov = (1/GetGameplayCamFov())*100
+    local scale = scale*fov
+
+    SetTextScale(scaleX*scale, scaleY*scale)
+    SetTextFont(fontId)
+    SetTextProportional(1)
+    SetTextColour(r, g, b, a)
+    SetTextDropshadow(0, 0, 0, 0, 255)
+    SetTextEdge(2, 0, 0, 0, 150)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(textInput)
+    SetDrawOrigin(x,y,z+2, 0)
+    DrawText(0.0, 0.0)
+    ClearDrawOrigin()
+end
+
+function Drawing.drawMissionText(m_text, showtime)
+    ClearPrints()
+    SetTextEntry_2("STRING")
+    AddTextComponentString(m_text)
+    DrawSubtitleTimed(showtime, 1)
+end
+
+function msginf(msg, duree)
+    duree = duree or 500
+    ClearPrints()
+    SetTextEntry_2("STRING")
+    AddTextComponentString(msg)
+    DrawSubtitleTimed(duree, 1)
+end
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(2)
+		local pos = GetEntityCoords(GetPlayerPed(-1), true)
+
+		for k, j in pairs(TeleportFromTo) do
+
+			--msginf(k .. " " .. tostring(j.positionFrom.x), 15000)
+			if(Vdist(pos.x, pos.y, pos.z, j.positionFrom.x, j.positionFrom.y, j.positionFrom.z) < 150.0)then
+				DrawMarker(1, j.positionFrom.x, j.positionFrom.y, j.positionFrom.z - 1, 0, 0, 0, 0, 0, 0, 1.0001, 1.0001, .801, 255, 255, 255,255, 0, 0, 0,0)
+				if(Vdist(pos.x, pos.y, pos.z, j.positionFrom.x, j.positionFrom.y, j.positionFrom.z) < 5.0)then
+					Drawing.draw3DText(j.positionFrom.x, j.positionFrom.y, j.positionFrom.z - 1.100, j.positionFrom.nom, 1, 0.2, 0.1, 255, 255, 255, 215)
+					if(Vdist(pos.x, pos.y, pos.z, j.positionFrom.x, j.positionFrom.y, j.positionFrom.z) < 2.0)then
+						ClearPrints()
+						SetTextEntry_2("STRING")
+						AddTextComponentString("Press ~r~E~w~ to ".. j.positionFrom.nom)
+						DrawSubtitleTimed(2000, 1)
+						if IsControlJustPressed(1, 38) then
+							DoScreenFadeOut(1000)
+							Citizen.Wait(2000)
+							SetEntityCoords(GetPlayerPed(-1), j.positionTo.x, j.positionTo.y, j.positionTo.z - 1)
+							DoScreenFadeIn(1000)
+						end
+					end
+				end
+			end
+
+			if(Vdist(pos.x, pos.y, pos.z, j.positionTo.x, j.positionTo.y, j.positionTo.z) < 150.0)then
+				DrawMarker(1, j.positionTo.x, j.positionTo.y, j.positionTo.z - 1, 0, 0, 0, 0, 0, 0, 1.0001, 1.0001, .801, 255, 255, 255,255, 0, 0, 0,0)
+				if(Vdist(pos.x, pos.y, pos.z, j.positionTo.x, j.positionTo.y, j.positionTo.z) < 5.0)then
+					Drawing.draw3DText(j.positionTo.x, j.positionTo.y, j.positionTo.z - 1.100, j.positionTo.nom, 1, 0.2, 0.1, 255, 255, 255, 215)
+					if(Vdist(pos.x, pos.y, pos.z, j.positionTo.x, j.positionTo.y, j.positionTo.z) < 2.0)then
+						ClearPrints()
+						SetTextEntry_2("STRING")
+						AddTextComponentString("Press ~r~E~w~ to ".. j.positionTo.nom)
+						DrawSubtitleTimed(2000, 1)
+						if IsControlJustPressed(1, 38) then
+							DoScreenFadeOut(1000)
+							Citizen.Wait(2000)
+							SetEntityCoords(GetPlayerPed(-1), j.positionFrom.x, j.positionFrom.y, j.positionFrom.z - 1)
+							DoScreenFadeIn(1000)
+						end
+					end
+				end
+			end
+		end
+	end
+end)
